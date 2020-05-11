e95822f53f010b79cc9a6dd1ceeabdfc615cdc3e
Aan <aansanova48@gmail.com>
2020-05-11 21:45:28 +0700

    edit

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,9 +1,28 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, minlength: 5 },
-  displayName: { type: String },
+  name: {
+    type: String,
+    maxlength: 50
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true, 
+    minlength: 5 
+  },
+  role: {
+    type: Number,
+    default: 0
+  },
+  cart: {
+    type: Array,
+    default: []
+  },
 });
 
 module.exports = User = mongoose.model("user", userSchema);
